ae11253c03a249e3cb0ddc47f49288fd7d4f248f
s.blagojevic <s.blagojevic0406@gmail.com>
2022-09-12 18:25:58 +0200

    resolve merge

--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -3,6 +3,6 @@ import {authSlice} from './auth.slice';
 import {imagesSlice} from './images.slice';
 
 export const rootReducer = combineReducers({
-  // auth: authSlice.reducer,
+  auth: authSlice.reducer,
   images: imagesSlice.reducer,
 });
