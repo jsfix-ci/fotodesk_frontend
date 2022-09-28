2999525d70be8de84d958523653c505c05aafcc9
s.blagojevic <s.blagojevic0406@gmail.com>
2022-09-28 20:37:21 +0200

    create-user-added

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {authApi} from '../../api';
 import Register from '../../components/Register/Register';
+import {authSlice} from '../../store/slices/auth.slice';
 
 export default function RegisterPage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const registerFields = ['firstName', 'lastName', 'displayName', 'email', 'password', 'repeatPassword'];
+  async function handleSubmit(payload: any) {
+    try {
+      const {data} = await authApi.register(payload);
+
+      dispatch(
+        authSlice.actions.login({
+          ...data,
+        })
+      );
+      localStorage.setItem('token', data.token);
+      navigate('/profile');
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <>
-      <Register isAdmin={true} />
+      <Register isAdmin={true} registerFields={registerFields} handleSubmit={handleSubmit} />
     </>
   );
 }
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,33 +1,34 @@
 import React, {useEffect, useState} from 'react';
-import {useDispatch} from 'react-redux';
-// import {useDispatch} from 'react-redux';
-import {useNavigate} from 'react-router-dom';
-import {authApi} from '../../api';
 import {IRegisterData} from '../../api/auth.api';
-import {authSlice} from '../../store/slices/auth.slice';
 import Form from '../Form/Form';
 import {formFields} from '../Form/form-utilities';
 
-// import {authApi} from '../../api';
-// import {authSlice} from '../../store/slices/auth.slice';
-// import {emailPattern,} from '../../utilities/helper';
-export default function Register({isAdmin}: any) {
-  const registerFields = ['firstName', 'lastName', 'displayName', 'email', 'password', 'repeatPassword'];
-  const registerFormFields = formFields.filter((formField) => registerFields.includes(formField.name));
-  const initialState = registerFormFields.reduce((a, b) => ({...a, [b.name]: {...b, value: '', onChange: handleChange}}), {});
+
+export default function Register({isAdmin,registerFields,handleSubmit, initialValues}: any) {
+  
+  const registerFormFields = formFields.filter((formField) => registerFields?.includes(formField.name));
+  const initialState = registerFormFields.reduce((a, b) => ({...a, [b.name]: {...b, value: initialValues? initialValues[b.name] : '', onChange: handleChange}}), {});
   const [enteredFormFIelds, setEnteredFormField] = useState<any>(initialState);
   const [isDisabled, setIsDisabled] = useState(true);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   function changeDisable(enteredFormFIelds: any) {
     let isDisabled = !Object.values(enteredFormFIelds)
-      .filter((field: any) => field.required)
-      .every((field: any) => !!field.value.length);
-    isDisabled = isDisabled || enteredFormFIelds.password.value !== enteredFormFIelds.repeatPassword.value;
+      .filter((field: any) => field?.required)
+      .every((field: any) => !!field?.value?.length);
+      if(enteredFormFIelds?.repeatPassword){
+
+        isDisabled = isDisabled || enteredFormFIelds?.password?.value !== enteredFormFIelds?.repeatPassword?.value;
+      }
     setIsDisabled(isDisabled);
   }
-
+  const onSubmit = (e:any)=>{
+    e.preventDefault()
+    const payload = Object.keys(enteredFormFIelds).reduce(
+      (acc, curr) => ({...acc, [curr]: enteredFormFIelds[curr]?.['value']}),
+      {}
+    ) as IRegisterData;
+    handleSubmit(payload)
+  }
   useEffect(() => {
     changeDisable(enteredFormFIelds);
   }, [enteredFormFIelds]);
@@ -37,26 +38,7 @@ export default function Register({isAdmin}: any) {
     setEnteredFormField((prevState: any) => ({...prevState, [name]: {...prevState[name], value: value.trim()}}));
   }
 
-  async function handleSubmit(event: any) {
-    try {
-      event.preventDefault();
-      const payload = Object.keys(enteredFormFIelds).reduce(
-        (acc, curr) => ({...acc, [curr]: enteredFormFIelds[curr]['value']}),
-        {}
-      ) as IRegisterData;
-      const {data} = await authApi.register(payload);
 
-      dispatch(
-        authSlice.actions.login({
-          ...data,
-        })
-      );
-      localStorage.setItem('token', data.token);
-      navigate('/profile');
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
-  return <Form formFields={enteredFormFIelds} handleChange={handleChange} handleSubmit={handleSubmit} isDisabled={isDisabled} />;
+  return <Form formFields={enteredFormFIelds} handleChange={handleChange} handleSubmit={onSubmit} isDisabled={isDisabled} />;
 }
