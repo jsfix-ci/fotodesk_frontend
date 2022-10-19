cbb5e1c63e6d7b34ad470858ce4ddfc08b3ba270
Aleksandar <76660686+AleksandarGajic01@users.noreply.github.com>
2022-10-19 14:06:23 +0200

    added toast messages

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -4,7 +4,7 @@ import {commonSlice} from '../store/slices/common.slice';
 import Messages from '../utilities/Messages';
 
 export default class BaseApi {
-  private request: AxiosInstance;
+  private request: AxiosInstance; 
   private readonly messages = new Messages(commonSlice.actions.setMessage);
   protected token: string | null = null;
   protected dispatch: any = StoreKeeper.store.dispatch;
@@ -27,15 +27,14 @@ export default class BaseApi {
       (success) => {
         clearTimeout(loadingTimeout);
         StoreKeeper.store.dispatch(commonSlice.actions.setIsLoading(false));
-        const key = success.config.method! + success.config.url?.split('/').at(-1) ?? '';
+        const key = success.config.method! + success.config.url?.split('/').filter((s:string)=>isNaN(Number(s))).at(-1) ?? '';
         this.messages.generateSuccess(key);
         return success;
       },
       (error) => {
         clearTimeout(loadingTimeout);
         StoreKeeper.store.dispatch(commonSlice.actions.setIsLoading(false));
-        console.log(error);
-        const key = error.config.method! + error.config.url?.split('/').at(-1) ?? '';
+        const key = error.config.method! + error.config.url?.split('/').filter((s:string)=>isNaN(Number(s))).at(-1) ?? '';
         this.messages.generateError(key);
         throw error;
       }
--- src/utilities/toast-messages.ts
+++ src/utilities/toast-messages.ts
@@ -9,6 +9,46 @@ export const messagesMap: {[key: string]: any} = {
     text: 'Pogresni podaci',
     type: TypeEnum.error,
   },
+  postregisterSuccess: {
+    text: 'Uspjesno ste se registrovali',
+    type: TypeEnum.success,
+  },
+  postregisterError: {
+    text: 'Unesite ispravne podatke',
+    type: TypeEnum.error,
+  },
+  postusersSuccess: {
+    text: 'Uspjesno ste dodali korisnika',
+    type: TypeEnum.success,
+  },
+  postusersError: {
+    text: 'Popunite sva polja',
+    type: TypeEnum.error,
+  },
+  putusersSuccess: {
+    text: 'Uspjesna akcija',
+    type: TypeEnum.success,
+  },
+  putusersError: {
+    text: 'Neuspjesna akcija',
+    type: TypeEnum.error,
+  },
+  deleteusersSuccess: {
+    text: 'Uspjesno ste obrisali korisnika',
+    type: TypeEnum.success,
+  },
+  deleteusersError: {
+    text: 'Neuspjesna akcija',
+    type: TypeEnum.error,
+  },
+  'putbulk-updateSuccess': {
+    text: 'Uspjesno ste dodali tag',
+    type: TypeEnum.success,
+  },
+  'putbulk-updateError': {
+    text: 'Neispravan unos',
+    type: TypeEnum.error,
+  },
   postimagesSuccess: {
     text: 'Uspjesno ste dodali sliku',
     type: TypeEnum.success,
@@ -17,31 +57,74 @@ export const messagesMap: {[key: string]: any} = {
     text: 'Greska pri ucitavanju',
     type: TypeEnum.error,
   },
-  'putbulk-updateSuccess': {
-    text: 'Uspjesno ste dodali tag',
+  putimagesSuccess: {
+    text:'Uspijesna akcija',
+    type: TypeEnum.success
+  },
+  putimagesError: {
+    text: 'Neuspijesna akcija',
+    type: TypeEnum.error
+  },
+  deleteimagesSuccess: {
+    text: 'Uspijesno ste obrisali sliku',
+    type: TypeEnum.success
+  },
+  deleteimagesError: {
+    text: 'Neuspijesna akcija',
+    type: TypeEnum.error
+  },
+  postwatermarksSuccess: {
+    text: 'Uspjesno ste dodali watermark',
     type: TypeEnum.success,
   },
-
-  'putbulk-updateError': {
-    text: 'Neispravan unos',
+  postwatermarksError: {
+    text: 'Greska pri ucitavanju',
     type: TypeEnum.error,
   },
-
-  postregisterSuccess: {
-    text: 'Uspjesno ste se registrovali',
+  putwatermarksSuccess: {
+    text: 'Uspjesna akcija',
     type: TypeEnum.success,
+  },
+  putwatermarksError: {
+    text: 'Neuspjesna akcija',
+    type: TypeEnum.error,
+  },
+  deletewatermarksSuccess: {
+    text: 'Uspjesno ste obrisali watermark',
+    type: TypeEnum.success,
+  },
+  deletewatermarksError: {
+    text: 'Neuspjesna akcija',
+    type: TypeEnum.error,
   },
 };
 
 export const successResponses: {[key: string]: string} = {
   postlogin: 'postloginSuccess',
-  postimages: 'postimagesSuccess',
-  'putbulk-update': 'putbulk-updateSuccess',
   postregister: 'postregisterSuccess',
+  postusers: 'postusersSuccess',
+  putusers: 'putusersSuccess',
+  deleteusers: 'deleteusersSuccess',
+  'putbulk-update': 'putbulk-updateSuccess',
+  postimages: 'postimagesSuccess',
+  putimages: 'putimagesSuccess',
+  deleteimages: 'deleteimagesSuccess',
+  postwatermarks: 'postwatermarksSuccess',
+  putwatermarks: 'putwatermarksSuccess',
+  deletewatermarks: 'deletewatermarksSuccess',
 };
 
 export const errorResponses: {[key: string]: string} = {
   postlogin: 'postloginError',
-  postimages: 'postimagesError',
+  postregister: 'postregisterError',
+  postusers: 'postusersError',
+  putusers: 'putusersError',
+  deleteusers: 'deleteusersError',
   'putbulk-update': 'putbulk-updateError',
+  postimages: 'postimagesError',
+  putimages: 'putimagesError',
+  deleteimages: 'deleteimagesError',
+  postwatermarks: 'postwatermarksError',
+  putwatermarks: 'putwatermarksError',
+  deletewatermarks: 'deletewatermarksError',
 };
